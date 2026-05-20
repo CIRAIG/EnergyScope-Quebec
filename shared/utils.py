442bee9386c91154d65aba8267e306a261e8ea10
matthieu-str <matthieu.souttre@gmail.com>
2026-05-20 11:44:51 -0400

    updated path + added load_snapshot function

--- shared/utils.py
+++ shared/utils.py
@@ -1,9 +1,17 @@
+from pathlib import Path
 from energyscope.models import Model
 from energyscope.energyscope import Energyscope
 from energyscope.result import postprocessing, Result
 
+# Get the absolute path to the directory containing this file
+_UTILS_DIR = Path(__file__).parent.absolute()
+_DATA_DIR = _UTILS_DIR / 'data'
+_MODEL_DIR = _UTILS_DIR / 'model'
+_SCENARIOS_DIR = _UTILS_DIR / 'scenarios'
+
 def run_model(
         model: Model,
+        apply_postprocessing: bool = False,
 ) -> Result:
     solver_options = {
         'solver': 'gurobi',
@@ -12,6 +20,40 @@ def run_model(
 
     es = Energyscope(model=model, solver_options=solver_options)
     res = es.calc()
-    res = postprocessing(res)
+    if apply_postprocessing:
+        res = postprocessing(res)
 
     return res
+
+
+def load_snapshot(year: int, scenario: bool = True) -> Model:
+
+    # The year 2020 is approximated with 2021 data, and 2025 with 2023 data (until we have more recent data)
+    if year == 2021:
+        year = 2020
+    elif year == 2023:
+        year = 2025
+
+    # To load a snapshot model using the transition framework, we set one unique time step
+    # Therefore, we re-write QC_set_snapshot.dat accordingly
+    snapshot_file = _DATA_DIR / 'QC_set_snapshot.dat'
+    with open(snapshot_file, 'w') as f:
+        f.write(f'set YEARS := YEAR_{year};\n')
+        f.write('set YEAR_ONE := ;\n')
+        f.write(f'set YEARS_WND := YEAR_{year};\n')
+        f.write(f'set YEARS_UP_TO := YEAR_{year};')
+
+    files = [
+        ('mod', str(_MODEL_DIR / 'QC_es_main.mod')),
+        ('mod', str(_MODEL_DIR / 'QC_objective_function.mod')),
+        ('dat', str(snapshot_file)),
+        ('dat', str(_DATA_DIR / 'QC_data.dat')),
+        ('dat', str(_DATA_DIR / 'Techs' / f'QC_techs_{year}.dat')),
+        ('dat', str(_DATA_DIR / 'Shares' / f'QC_shares_{year}.dat')),
+        ('dat', str(_DATA_DIR / 'EUD' / f'QC_eud_{year}.dat')),
+    ]
+
+    if year == 2050 and scenario:
+        files.append(('dat', str(_SCENARIOS_DIR / f'QC_generic_scenario_{year}.dat')))
+
+    return Model(files)
